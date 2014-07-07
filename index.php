bad4935052b4d27ba538c43ee417cde8672b2412
Manish Goyal <manishgo81@gmail.com>
2014-07-07 00:57:00 -0400

    Added Logic

--- index.php
+++ index.php
@@ -3,31 +3,46 @@
 
 <head>
   <title>Manish Goyal P2</title>
+  <meta charset="utf-8">
   <?php require('logic.php'); ?>
 </head>
 
-<body align="center">
-  <?php echo $output ?><br/><br/>
+<body>
+    <div align="center" id='output'>
+        <h1>Password Generator</h1>
+        <h2><?php echo $OutputPassword; ?></h2>
+        
+    </div>
 
-  <form action="logic.php" method="get">
-    Number of words <select name = "WordCount">
-      <option value="1"> 1 </option>
-      <option value="2"> 2 </option>
-      <option value="3"> 3 </option>
-      <option value="4" selected = "selected"> 4 </option>
-      <option value="5"> 5 </option>
-      <option value="6"> 6 </option>
-      <option value="7"> 7 </option>
-      <option value="8"> 8 </option>
-      <option value="9"> 9 </option>
-    </select><br/>
+    <div align="center"m id="Form1">
 
-     <input type="checkbox" name="NumberFlag"style="float:center"/>Include Number
-     <input type="checkbox" name="SymbolFlag"style="float:center"/>Include Symbols 
-     <input type="checkbox" name="UpperCaseFlag"style="float:center"/>UpperCase First Letter <br/><br/>
-     <input type="submit" style="float:center"/>
-     <input type="reset" style="float:center"/>
-  </form>  <p align="center"> <img src = "http://imgs.xkcd.com/comics/password_strength.png" hight='400' width='500'> </p>
+        <form action="logic.php" method="GET">
+            Number of words <select name = "WordCount">
+              <option value="1"> 1 </option>
+              <option value="2"> 2 </option>
+              <option value="3"> 3 </option>
+              <option value="4" selected = "selected"> 4 </option>
+              <option value="5"> 5 </option>
+              <option value="6"> 6 </option>
+              <option value="7"> 7 </option>
+              <option value="8"> 8 </option>
+              <option value="9"> 9 </option>
+            </select><br/>
+
+            <input type="checkbox" name="NumberFlag"/>Include Number
+            <input type="checkbox" name="SymbolFlag"/>Include Symbols 
+            <input type="checkbox" name="UpperCaseFlag"/>UpperCase First Letter <br/><br/>
+            <input type="submit" name="submit" onClick="myFunction()"/>
+            <input type="reset"/>
+        </form> 
+
+    </div><br/><br/><br/><br/><br/><br/>
+
+    <div align="center">
+      <img src = "http://imgs.xkcd.com/comics/password_strength.png" hight='400' width='500' style="float:center">
+    </div>
+
+    
 </body>
 
 </html>
--- logic.php
+++ logic.php
@@ -1,4 +1,67 @@
 <?php 
+date_default_timezone_set('America/New_York');
 
+$WordCnt = 0;
+$numFlag = 'off';
+$symFlag = 'off';
+$caseFlag = 'off';
 
+$fileHandle = fopen("WordFile.txt", "r");
 
+$WordArray = file("WordFile.txt");
+$OutputPassword='';
+
+function myFunction()
+{
+
+	foreach ($_GET as $key => $value) {
+		if($key=="WordCount")
+			$WordCnt = $value;
+		else if ($key=="NumberFlag") {
+			$numFlag = $value;
+		}
+		else if ($key=="SymbolFlag") {
+			$symFlag = $value;
+		}
+		else if ($key=="UpperCaseFlag") {
+			$caseFlag = $value;
+		}
+	}
+
+	for($i=0; $i<$WordCnt; ++$i)
+	{
+		$currRandNum = rand(0,4999);
+		$result = $WordArray[$currRandNum];
+		if($i==0 && $caseFlag == 'on')
+		{
+			$firstChar = $result[0];
+			$result[0] = chr(ord($firstChar)-32);
+		}
+
+		$result = substr($result,0,(strlen($result)-2));
+		$OutputPassword .= $result;
+		if($i<($WordCnt-1))
+			$OutputPassword.='-';
+	}
+
+	if($symFlag == 'on')
+	{
+		//$OutputPassword.='-';
+		$symNum = rand(1,32);
+		if($symNum<16)
+			$OutputPassword.=chr(32+$symNum);
+		else if($symNum<23)
+			$OutputPassword.=chr(42+$symNum);
+		else if($symNum<29)
+			$OutputPassword.=chr(68+$symNum);
+		else
+			$OutputPassword.=chr(94+$symNum);
+	}
+
+	if($numFlag=='on')
+	{
+		//$OutputPassword.='-';
+		$Num = rand(0,9);
+		$OutputPassword .= $Num;
+	}
+}
